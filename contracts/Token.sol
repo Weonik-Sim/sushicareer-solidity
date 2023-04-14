422577ae4f1d865b931c01ec842966bdc8ed1239
sarah_sim <sarah_sim@MacBook-Pro.local>
2023-04-15 02:17:51 +0900

    Only User

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -12,120 +12,137 @@ contract Token {
     string public symbol = "SSH";
 
     // Company
-    struct Company {
-        address companyAddress;
-        string companyName;
-        uint256 companyToken;
-        uint employeeCount;
-    }
-    Company[] public companies;
+    // struct Company {
+    //     address companyAddress;
+    //     string companyName;
+    //     uint256 companyToken;
+    //     uint employeeCount;
+    // }
+    // Company[] public companies;
 
-    mapping(uint => address) public companyOwner;
-    mapping(address => uint) public ownerCompanyId;
-    mapping(address => bool) public companyExist;
-    mapping(string => uint) public companyNameId;
-    event UpdateCompanyName(uint id, string companyName);
+    // mapping(uint => address) public companyOwner;
+    // mapping(address => uint) public ownerCompanyId;
+    // mapping(address => bool) public companyExist;
+    // mapping(string => uint) public companyNameId;
+    // event UpdateCompanyName(uint id, string companyName);
 
     // Employee
     struct Employee {
         address employeeAddress;
         string employeeName;
-        uint256 employeeToken;
+        uint256 employeeZanToken;
         string companyName;
+        string companyUrl;
+        uint256 employeeSendToken;
+        uint256 employeeReceiveToken;
     }
 
     Employee[] public employees;
-    mapping(address => uint) public employeeId;         // employeesのINDEX
+    mapping(address => uint) public employeeAddressId;         // employeesのINDEX
     mapping(address => bool) public employeeExist;      // 既に登録済みかどうか
-    mapping(address => bool) public employeeCompanyExist;  // 会社に所属しているかどうか
-    mapping(address => uint) public employeeCompanyId;  // 会社に所属する時に設定
-    event RegisterEmployee(uint id, string userName, uint256 employeeToken);
+    mapping(uint => address) public employeeIdAddress;    // employeesのINDEXからアドレスを取得
+    // mapping(address => bool) public employeeCompanyExist;  // 会社に所属しているかどうか
+    // mapping(address => uint) public employeeCompanyId;  // 会社に所属する時に設定
+    event RegisterEmployee(uint id, string userName, uint256 employeeZanToken);
 
     // ユーザー登録
-    function _createEmployee(string memory _name) public {
+    function _createEmployee(string memory _name, string memory _companyName, string memory _companyUrl) public {
         require(employeeExist[msg.sender] == false, "Employee already exists");
-        employees.push(Employee(msg.sender, _name, 1000, ""));
+        employees.push(Employee(msg.sender, _name, 1000, _companyName, _companyUrl, 0, 0));
         uint id = employees.length - 1;
-        employeeId[msg.sender] = id;
+        employeeAddressId[msg.sender] = id;
+        employeeIdAddress[id] = msg.sender;
         employeeExist[msg.sender] = true;
-        employeeCompanyExist[msg.sender] = false;
-        emit RegisterEmployee(id, _name, employees[id].employeeToken);
+        // employeeCompanyExist[msg.sender] = false;
+        emit RegisterEmployee(id, _name, employees[id].employeeZanToken);
     }
 
     // ユーザーの情報を取得
-    function _getEmployeeInfo() public view returns (string memory, uint256, string memory) {
+    function _getEmployeeInfo() public view returns (Employee memory) {
         // require(employeeExist[msg.sender] == true, "Employee not exists");
         require(employeeExist[msg.sender] == true, string(abi.encodePacked("Employee not exists: ", msg.sender)));
-        uint id = employeeId[msg.sender];
-        string memory companyName = "";
-        if (employeeCompanyExist[msg.sender] != false) {
-            uint companyId = employeeCompanyId[msg.sender];
-            companyName = companies[companyId].companyName;
-        }
+        uint id = employeeAddressId[msg.sender];
+        // string memory companyName = "";
+        // if (employeeCompanyExist[msg.sender] != false) {
+        //     uint companyId = employeeCompanyId[msg.sender];
+        // }
 
-        return (employees[id].employeeName, employees[id].employeeToken, companyName);
+        return employees[id];
+    }
+
+    function _sendSushi(uint256 _to, uint256 _sushi) public {
+        require(employeeExist[msg.sender] == true, "Employee already exists");
+        require(employeeExist[employeeIdAddress[_to]] == true, "Employee already exists");
+        require(employees[employeeAddressId[msg.sender]].employeeZanToken >= _sushi, "Employee already exists");
+        employees[employeeAddressId[msg.sender]].employeeZanToken -= _sushi;
+        employees[employeeAddressId[msg.sender]].employeeSendToken += _sushi;
+        employees[_to].employeeZanToken += _sushi;
+        employees[_to].employeeReceiveToken += _sushi;
+
+        emit RegisterEmployee(employeeAddressId[msg.sender], employees[employeeAddressId[msg.sender]].employeeName, employees[employeeAddressId[msg.sender]].employeeZanToken);
+        emit RegisterEmployee(_to, employees[_to].employeeName, employees[_to].employeeZanToken);
     }
 
     // ユーザーが会社に所属する
-    function _setEmployeeJoinCompany(uint _companyId) public {
-        require(employeeCompanyExist[msg.sender] == false, "Employee already join company");
-        employeeCompanyId[msg.sender] = _companyId;
-        employeeCompanyExist[msg.sender] = true;
+    // function _setEmployeeJoinCompany(uint _companyId) public {
+    //     require(employeeCompanyExist[msg.sender] == false, "Employee already join company");
+    //     employeeCompanyId[msg.sender] = _companyId;
+    //     employeeCompanyExist[msg.sender] = true;
 
-        companies[_companyId].employeeCount += 1;
-        companies[_companyId].companyToken += employees[employeeId[msg.sender]].employeeToken;
-        employees[employeeId[msg.sender]].employeeToken = 0;
-        employees[employeeId[msg.sender]].companyName = companies[_companyId].companyName;
-        emit RegisterEmployee(employeeId[msg.sender], employees[employeeId[msg.sender]].employeeName, employees[employeeId[msg.sender]].employeeToken);
-    }
+    //     companies[_companyId].employeeCount += 1;
+    //     companies[_companyId].companyToken += employees[employeeId[msg.sender]].employeeToken;
+    //     employees[employeeId[msg.sender]].employeeToken = 0;
+    //     employees[employeeId[msg.sender]].companyName = companies[_companyId].companyName;
+    //     emit RegisterEmployee(employeeId[msg.sender], employees[employeeId[msg.sender]].employeeName, employees[employeeId[msg.sender]].employeeToken);
+    // }
 
     // ユーザーが退職する
-    function _setEmployeeLeaveCompany(uint _companyId, uint256 _zanToken) public {
-        require(employeeCompanyExist[msg.sender] == true, "Employee not join company");
-        employeeCompanyId[msg.sender] = 0;
-        employeeCompanyExist[msg.sender] = false;
+    // function _setEmployeeLeaveCompany(uint _companyId, uint256 _zanToken) public {
+    //     require(employeeCompanyExist[msg.sender] == true, "Employee not join company");
+    //     employeeCompanyId[msg.sender] = 0;
+    //     employeeCompanyExist[msg.sender] = false;
 
-        companies[_companyId].employeeCount -= 1;
-        companies[_companyId].companyToken -= _zanToken;
-        employees[employeeId[msg.sender]].employeeToken = _zanToken;
-        employees[employeeId[msg.sender]].companyName = "";
-        emit RegisterEmployee(employeeId[msg.sender], employees[employeeId[msg.sender]].employeeName, employees[employeeId[msg.sender]].employeeToken);
-    }
+    //     companies[_companyId].employeeCount -= 1;
+    //     companies[_companyId].companyToken -= _zanToken;
+    //     employees[employeeId[msg.sender]].employeeToken = _zanToken;
+    //     employees[employeeId[msg.sender]].companyName = "";
+    //     emit RegisterEmployee(employeeId[msg.sender], employees[employeeId[msg.sender]].employeeName, employees[employeeId[msg.sender]].employeeToken);
+    // }
 
     // 全ての情報を取得
-    function _getAllThings() public view returns (Company[] memory, Employee[] memory) {
-        return (companies, employees);
+    function _getAllThings() public view returns (Employee[] memory) {
+        return employees;
     }
 
 ////////////////////////////////////////////////////////////////////////////////////////
 
     // 会社登録
-    function _createCompany(string memory _name) public {
-        require(companyExist[msg.sender] == false, "Company already exists");
-        companies.push(Company(msg.sender, _name, 0, 0));
-        uint id = companies.length - 1;
-        companyOwner[id] = msg.sender;
-        ownerCompanyId[msg.sender] = id;
-        companyNameId[_name] = id;
-        companyExist[msg.sender] = true;
-        emit UpdateCompanyName(id, _name);
-    }
+    // function _createCompany(string memory _name) public {
+    //     require(companyExist[msg.sender] == false, "Company already exists");
+    //     companies.push(Company(msg.sender, _name, 0, 0));
+    //     uint id = companies.length - 1;
+    //     companyOwner[id] = msg.sender;
+    //     ownerCompanyId[msg.sender] = id;
+    //     companyNameId[_name] = id;
+    //     companyExist[msg.sender] = true;
+    //     emit UpdateCompanyName(id, _name);
+    // }
 
-    // 会社のトークンを取得
-    function _getCompanyToken() public view returns (uint256, address) {
-        require(companyExist[msg.sender] == true, "Company not exists");
-        uint id = ownerCompanyId[msg.sender];
-        return (companies[id].companyToken, msg.sender);
-    }
+    // // 会社のトークンを取得
+    // function _getCompanyToken() public view returns (uint256, address) {
+    //     require(companyExist[msg.sender] == true, "Company not exists");
+    //     uint id = ownerCompanyId[msg.sender];
+    //     return (companies[id].companyToken, msg.sender);
+    // }
 
-    // 会社名を取得
-    function _getCompanyName() public view returns (string memory) {
-        require(companyExist[msg.sender] == true, "Company not exists");
-        uint id = ownerCompanyId[msg.sender];
-        return companies[id].companyName;
-    }
+    // // 会社名を取得
+    // function _getCompanyName() public view returns (string memory) {
+    //     require(companyExist[msg.sender] == true, "Company not exists");
+    //     uint id = ownerCompanyId[msg.sender];
+    //     return companies[id].companyName;
+    // }
 
-    function _getAllCompanyInfo() public view returns (Company[] memory) {
-        return companies;
-    }
+    // function _getAllCompanyInfo() public view returns (Company[] memory) {
+    //     return companies;
+    // }
 }
